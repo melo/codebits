7b7a2600df1049ceeb1f29881f628796b596a575
Pedro Melo <melo@simplicidade.org>
2008-11-13 12:32:22 +0000

    Allow extra options to the bot constructor
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- bots/process_sync/All.pm
+++ bots/process_sync/All.pm
@@ -4,18 +4,22 @@ use strict;
 use warnings;
 
 sub connect_all {
-  my ($class, $bot_class) = @_;
+  my ($class, $bot_class, %extra_args) = @_;
   my @bots;
   
   for(my $n = 1; $n <= ($ENV{BOT_COUNT} || 5); $n++) {
-    my $bot = $bot_class->new(
+    my %bot_args = (
       jid      => "sync$n\@test.simplicidade.org",
       password => 'teste',
       host     => '127.0.0.1',
       port     => 5222,
 
       http_port => 3004 + $n,
+      
+      %extra_args,
     );
+    
+    my $bot = $bot_class->new(%bot_args);
     $bot->start;
     
     push @bots, $bot;
