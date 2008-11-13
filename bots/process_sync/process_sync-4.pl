f06bbcd756c67d770206a2fcb953c01d90193f5d
Pedro Melo <melo@simplicidade.org>
2008-11-13 13:14:39 +0000

    Add pid an yo commands
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- bots/process_sync/process_sync-4.pl
+++ bots/process_sync/process_sync-4.pl
@@ -9,6 +9,26 @@ use strict;
 use warnings;
 use base 'Bot2';
 
+sub muc_handle_command {
+  my ($self, $room, $msg, $cmd) = @_;
+  my $reply;
+  
+  if ($cmd =~ m/^\s*pid\s*[?!]?\s*$/i) {
+    $reply = $msg->make_reply;
+    $reply->add_body("My pid is $$.");
+  }
+  elsif ($cmd =~ m/^\s*yo\s*[?!]?\s*$/i) {
+    $reply = $msg->make_reply;
+    $reply->add_body("Yo-Yo-Ma Rules!");
+  }
+  
+  if ($reply) {
+    $reply->send;
+  }
+  else {
+    $self->muc_handle_command_unknown($room, $msg, $cmd);
+  }
+}
 
 package main;
 
